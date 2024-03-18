1d20f345a4061ebb3c5dadac514897629ac573b5
timcki <me@timcki.com>
2024-03-18 11:24:22 +0100

    feat: configure nushell

---
+++ config.nu
@@ -0,0 +1,173 @@
+let dark_theme = {
+    # color for nushell primitives
+    separator: white
+    leading_trailing_space_bg: { attr: n } # no fg, no bg, attr none effectively turns this off
+    header: green_bold
+    empty: blue
+    # Closures can be used to choose colors for specific values.
+    # The value (in this case, a bool) is piped into the closure.
+    # eg) {|| if $in { 'light_cyan' } else { 'light_gray' } }
+    bool: light_cyan
+    int: white
+    filesize: cyan
+    duration: white
+    date: purple
+    range: white
+    float: white
+    string: white
+    nothing: white
+    binary: white
+    cell-path: white
+    row_index: green_bold
+    record: white
+    list: white
+    block: white
+    hints: dark_gray
+    search_result: {bg: red fg: white}
+    shape_and: purple_bold
+    shape_binary: purple_bold
+    shape_block: blue_bold
+    shape_bool: light_cyan
+    shape_closure: green_bold
+    shape_custom: green
+    shape_datetime: cyan_bold
+    shape_directory: cyan
+    shape_external: cyan
+    shape_externalarg: green_bold
+    shape_external_resolved: light_yellow_bold
+    shape_filepath: cyan
+    shape_flag: blue_bold
+    shape_float: purple_bold
+    # shapes are used to change the cli syntax highlighting
+    shape_garbage: { fg: white bg: red attr: b}
+    shape_globpattern: cyan_bold
+    shape_int: purple_bold
+    shape_internalcall: cyan_bold
+    shape_keyword: cyan_bold
+    shape_list: cyan_bold
+    shape_literal: blue
+    shape_match_pattern: green
+    shape_matching_brackets: { attr: u }
+    shape_nothing: light_cyan
+    shape_operator: yellow
+    shape_or: purple_bold
+    shape_pipe: purple_bold
+    shape_range: yellow_bold
+    shape_record: cyan_bold
+    shape_redirection: purple_bold
+    shape_signature: green_bold
+    shape_string: green
+    shape_string_interpolation: cyan_bold
+    shape_table: blue_bold
+    shape_variable: purple
+    shape_vardecl: purple
+}
+
+
+$env.PROMPT_INDICATOR = ""
+$env.PROMPT_INDICATOR_VI_INSERT = "➜ "
+$env.PROMPT_INDICATOR_VI_NORMAL = "← "
+$env.PROMPT_MULTILINE_INDICATOR = "::: "
+
+let carapace_completer = { |spans| carapace $spans.0 nushell $spans | from json }
+
+# The default config record. This is where much of your global configuration is setup.
+$env.config = {
+    show_banner: false # true or false to enable or disable the welcome banner at startup
+
+    ls: {
+        use_ls_colors: true # use the LS_COLORS environment variable to colorize output
+        clickable_links: true # enable or disable clickable links. Your terminal has to support links.
+    }
+
+    rm: {
+        always_trash: true # always act as if -t was given. Can be overridden with -p
+    }
+
+    table: {
+        mode: rounded # basic, compact, compact_double, light, thin, with_love, rounded, reinforced, heavy, none, other
+        index_mode: always # "always" show indexes, "never" show indexes, "auto" = show indexes when a table has "index" column
+        show_empty: true # show 'empty list' and 'empty record' placeholders for command output
+        padding: { left: 1, right: 1 } # a left right padding of each column in a table
+        trim: {
+            methodology: wrapping # wrapping or truncating
+            wrapping_try_keep_words: true # A strategy used by the 'wrapping' methodology
+            truncating_suffix: "..." # A suffix used by the 'truncating' methodology
+        }
+        header_on_separator: false # show header text on separator/border line
+        # abbreviated_row_count: 10 # limit data rows from top and bottom after reaching a set point
+    }
+
+    error_style: "fancy" # "fancy" or "plain" for screen reader-friendly error messages
+
+    # datetime_format determines what a datetime rendered in the shell would look like.
+    # Behavior without this configuration point will be to "humanize" the datetime display,
+    # showing something like "a day ago."
+    datetime_format: {
+        # normal: '%a, %d %b %Y %H:%M:%S %z'    # shows up in displays of variables or other datetime's outside of tables
+        # table: '%m/%d/%y %I:%M:%S%p'          # generally shows up in tabular outputs such as ls. commenting this out will change it to the default human readable datetime format
+    }
+
+    explore: {
+        status_bar_background: {fg: "#1D1F21", bg: "#C4C9C6"},
+        command_bar_text: {fg: "#C4C9C6"},
+        highlight: {fg: "black", bg: "yellow"},
+        status: {
+            error: {fg: "white", bg: "red"},
+            warn: {}
+            info: {}
+        },
+        table: {
+            split_line: {fg: "#404040"},
+            selected_cell: {bg: light_blue},
+            selected_row: {},
+            selected_column: {},
+        },
+    }
+
+    history: {
+        max_size: 100_000 # Session has to be reloaded for this to take effect
+        sync_on_enter: true # Enable to share history between multiple sessions, else you have to close the session to write history to file
+        file_format: "plaintext" # "sqlite" or "plaintext"
+        isolation: false # only available with sqlite file_format. true enables history isolation, false disables it. true will allow the history to be isolated to the current session using up/down arrows. false will allow the history to be shared across all sessions.
+    }
+
+    completions: {
+        case_sensitive: false # set to true to enable case-sensitive completions
+        quick: true    # set this to false to prevent auto-selecting completions when only one remains
+        partial: true    # set this to false to prevent partial filling of the prompt
+        algorithm: "fuzzy"    # prefix or fuzzy
+        external: {
+            enable: true # set to false to prevent nushell looking into $env.PATH to find more suggestions, `false` recommended for WSL users as this look up may be very slow
+            max_results: 100 # setting it lower can improve completion performance at the cost of omitting some options
+            completer: $carapace_completer # check 'carapace_completer' above as an example
+        }
+    }
+
+    filesize: {
+        metric: false # true => KB, MB, GB (ISO standard), false => KiB, MiB, GiB (Windows standard)
+        format: "auto" # b, kb, kib, mb, mib, gb, gib, tb, tib, pb, pib, eb, eib, auto
+    }
+
+    cursor_shape: {
+        emacs: line # block, underscore, line, blink_block, blink_underscore, blink_line, inherit to skip setting cursor shape (line is the default)
+        vi_insert: blink_block # block, underscore, line, blink_block, blink_underscore, blink_line, inherit to skip setting cursor shape (block is the default)
+        vi_normal: blink_block # block, underscore, line, blink_block, blink_underscore, blink_line, inherit to skip setting cursor shape (underscore is the default)
+    }
+
+    color_config: $dark_theme # if you want a more interesting theme, you can replace the empty record with `$dark_theme`, `$light_theme` or another custom record
+    use_grid_icons: true
+    footer_mode: "25" # always, never, number_of_rows, auto
+    float_precision: 2 # the precision for displaying floats in tables
+    buffer_editor: "" # command that will be used to edit the current line buffer with ctrl+o, if unset fallback to $env.EDITOR and $env.VISUAL
+    use_ansi_coloring: true
+    bracketed_paste: true # enable bracketed paste, currently useless on windows
+    edit_mode: vi # emacs, vi
+    render_right_prompt_on_last_line: false # true or false to enable or disable right prompt to be rendered on last line of the prompt.
+    use_kitty_protocol: false # enables keyboard enhancement protocol implemented by kitty console, only if your terminal support this.
+    highlight_resolved_externals: false # true enables highlighting of external commands in the repl resolved by which.
+
+    plugins: {} # Per-plugin configuration. See https://www.nushell.sh/contributor-book/plugins.html#configuration.
+}
+
+# source ~/.zoxide.nu
--- flake.nix
+++ flake.nix
@@ -1,159 +1,199 @@
-
 {
-description = "tim's system config";
+    description = "tim's system config";
 
-inputs = {
-nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-nix-darwin = {
-url = "github:lnl7/nix-darwin";
-inputs.nixpkgs.follows = "nixpkgs";
-};
-home-manager = {
-url = "github:nix-community/home-manager";
-inputs.nixpkgs.follows = "nixpkgs";
-};
-};
-
-outputs = inputs@{ self, nix-darwin, nixpkgs, home-manager }:
-let
-configuration = { pkgs, ... }: {
-
-services.nix-daemon.enable = true;
-# enable sudo with touchid
-security.pam.enablesudotouchidauth = true;
-
-# necessary for using flakes on this system.
-nix.settings.experimental-features = "nix-command flakes";
-
-system.configurationrevision = self.rev or self.dirtyrev or null;
-
-# used for backwards compatibility. please read the changelog
-# before changing: `darwin-rebuild changelog`.
-system.stateversion = 4;
-
-# the platform the configuration will be used on.
-nixpkgs.hostplatform = "aarch64-darwin";
-
-# declare the user that will be running `nix-darwin`.
-users.users.tim = {
-    name = "tim";
-    home = "/users/tim";
-
-    shell = pkgs.nushell;
-};
-
-programs.fish.enable = true;
-
-environment.systempackages = [
-    pkgs.neofetch
-    pkgs.alacritty
-];
-
-homebrew = {
-    enable = true;
-    onactivation.cleanup = "uninstall";
-
-    taps = [ ];
-    brews = [ "hyper-focus" ];
-    casks = [ "rectangle" ];
-};
-
-};
-homeconfig = { pkgs, ... }: {
-# this is internal compatibility configuration
-# for home-manager, don't change this!
-home.stateversion = "23.05";
-# let home-manager install and manage itself.
-programs.home-manager.enable = true;
-
-home.packages = with pkgs; [
-    fish
-    nushell
-    helix
-    bat
-    eza
-    fzf
-    git
-    go
-    jq
-    ripgrep
-    starship
-    tmux
-    zoxide
-    carapace
-];
-
-home.sessionvariables = { editor = "hx"; };
-
-programs = {
-
-    nushell = {
-    enable = true;
-    extraconfig = ''
-        let carapace_completer = {|spans|
-            carapace $spans.0 nushell $spans | from json
-        }
-        $env.config = {
-            edit_mode: vi,
-            show_banner: false,
-            completions: {
-            case_sensitive: false # case-sensitive completions
-            quick: true    # set to false to prevent auto-selecting completions
-            partial: true    # set to false to prevent partial filling of the prompt
-            algorithm: "fuzzy"    # prefix or fuzzy
-            external: {
-                # set to false to prevent nushell looking into $env.path to find more suggestions
-                enable: true
-                # set to lower can improve completion performance at the cost of omitting some options
-                max_results: 100
-                completer: $carapace_completer # check 'carapace_completer'
-                }
-            }
-        }
-        $env.path = (
-            $env.path |
-            split row (char esep) |
-        prepend /usr/local/bin |
-        append /usr/bin/env
-        )
-    '';
-    shellaliases = {
-        vi = "hx";
-        vim = "hx";
-        nano = "hx";
-
-        cat = "bat";
-    };
-
-    };
-    carapace = {
-        enable = true;
-        enableNushellIntegration = true;
-    };
-    starship = {
-        enable = true;
-        settings = {
-            add_newline = false;
-            format = "$directory $jj_status $sudo";
-
-            line_break.disabled = true;
-            jj_status.symbol = "";
+    inputs = {
+        nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+        nix-darwin = {
+            url = "github:lnl7/nix-darwin";
+            inputs.nixpkgs.follows = "nixpkgs";
+        };
+        home-manager = {
+            url = "github:nix-community/home-manager";
+            inputs.nixpkgs.follows = "nixpkgs";
         };
     };
-};
-};
-in {
-darwinconfigurations.bigboi = nix-darwin.lib.darwinsystem {
-modules = [
-    configuration
-    home-manager.darwinmodules.home-manager
-    {
-    home-manager.useglobalpkgs = true;
-    home-manager.useuserpackages = true;
-    home-manager.verbose = true;
-    home-manager.users.tim = homeconfig;
-    }
-];
-};
-};
+
+    outputs = inputs@{ self, nix-darwin, nixpkgs, home-manager }:
+    let
+    shared-configuration = { pkgs, ... }: {
+       services.nix-daemon.enable = true;
+        # enable sudo with touchid
+        security.pam.enableSudoTouchIdAuth = true;
+
+        # necessary for using flakes on this system.
+        nix.settings.experimental-features = "nix-command flakes";
+
+        system.configurationRevision = self.rev or self.dirtyrev or null;
+
+        # used for backwards compatibility. please read the changelog
+        # before changing: `darwin-rebuild changelog`.
+        system.stateVersion = 4;
+
+        # the platform the configuration will be used on.
+        nixpkgs.hostPlatform = "aarch64-darwin";
+
+        # declare the user that will be running `nix-darwin`.
+        users.users.tim = {
+            name = "tim";
+            home = "/users/tim";
+
+            shell = pkgs.nushell;
+        };
+
+        programs = {
+            fish.enable = true;
+        };
+
+        environment.systemPackages = [ pkgs.alacritty pkgs.nushell ];
+
+        homebrew = {
+            enable = true;
+            onActivation.cleanup = "uninstall";
+
+            casks = [ "rectangle" "zed" "discord" ];
+        };
+    };
+
+    bigboi-configuration = { pkgs, ... }: {
+        homebrew = {
+            taps = [ ];
+            brews = [ "hyper-focus" ];
+        };
+    };
+
+    small-configuration = { pkgs, ... }: {
+        homebrew = {
+            taps = [ ];
+            brews = [ ];
+        };
+    };
+
+    homeconfig = { pkgs, osConfig, config, ... }: {
+        # this is internal compatibility configuration
+        # for home-manager, don't change this!
+        home.stateVersion = "23.05";
+        # let home-manager install and manage itself.
+        programs.home-manager.enable = true;
+
+        home.packages = with pkgs; [
+            fish
+            nushell
+            helix
+            bat
+            eza
+            fzf
+            git
+            go
+            jq
+            ripgrep
+            starship
+            tmux
+            zoxide
+            carapace
+            jujutsu
+        ];
+
+        home.sessionVariables = { editor = "hx"; };
+
+        programs = {
+            nushell = {
+                enable = true;
+                configFile.source = ./config.nu;
+                extraConfig = ''
+                    $env.PATH = (
+                        $env.PATH |
+                        split row (char esep) |
+                        prepend /usr/local/bin |
+                        append /usr/bin/env
+                    )
+                '';
+                envFile.text = nixpkgs.lib.optionalString (osConfig ? environment) ''
+                    $env.PATH = ${builtins.replaceStrings
+                    [ "$USER" "$HOME" ]
+                    [ config.home.username config.home.homeDirectory ]
+                    osConfig.environment.systemPath}
+                '';
+                
+                shellAliases = {
+                    vi = "hx";
+                    vim = "hx";
+                    nano = "hx";
+
+                    switch = "darwin-rebuild switch --flake ~/.config/nix";
+
+                    cat = "bat";
+
+                    gs = "git status";
+                };
+
+            };
+
+            carapace = {
+                enable = true;
+                enableNushellIntegration = true;
+            };
+
+            starship = {
+                enable = true;
+                settings = {
+                    add_newline = false;
+                    format = "$directory $jj_status $sudo";
+
+                    line_break.disabled = true;
+                    jj_status.symbol = "";
+                };
+            };
+
+            
+            git = {
+                enable = true;
+
+                userName = "Tim Chmielecki";
+                userEmail = "me@timcki.com";
+                ignores = [ ".DS_Store" ".jj" ".nova" ".zed" ];
+
+                extraConfig = {
+                    init.defaultBranch = "main";
+                    push.autoSetupRemote = true;
+                    credential.helper = "osxkeychain";
+                    user.signingkey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIF3zNKM+CpNS5isd8MkCbPy6qTYbPlbVeyqm3hG5FYww";
+                    commit.gpgsign = true;
+                    gpg.format = "ssh";
+                    merge.conflictstyle = "diff3";
+                    diff.colormoved = "default";
+                };
+            };
+        };
+
+    };
+    in {
+        darwinConfigurations = {
+            bigboi = nix-darwin.lib.darwinSystem {
+                modules = [
+                    shared-configuration
+                    bigboi-configuration
+                    home-manager.darwinModules.home-manager {
+                        home-manager.useGlobalPkgs = true;
+                        home-manager.useUserPackages = true;
+                        home-manager.verbose = true;
+                        home-manager.users.tim = homeconfig;
+                    }
+                ];
+            };
+
+            small = nix-darwin.lib.darwinSystem {
+                modules = [
+                    shared-configuration
+                    small-configuration
+                    home-manager.darwinModules.home-manager {
+                        home-manager.useGlobalPkgs = true;
+                        home-manager.useUserPackages = true;
+                        home-manager.verbose = true;
+                        home-manager.users.tim = homeconfig;
+                    }
+                ];
+            };
+        };
+    };
+
 }
