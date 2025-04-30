bcdd786c287ee943ac5c3be6459f54b5a8f0126d
Tim Chmielecki <me@timcki.com>
2025-04-30 16:20:03 +0200

    jj: port config to nix

--- flake.nix
+++ flake.nix
@@ -20,7 +20,9 @@
         # enable sudo with touchid
         security.pam.services.sudo_local.touchIdAuth = true;
 
+
         # necessary for using flakes on this system.
+        ids.gids.nixbld     = 350;
         nix.settings.experimental-features = "nix-command flakes";
 
         nix.gc = {
@@ -213,10 +215,56 @@
                 };
             };
             zoxide.enable = true;
-            # carapace = {
-            #     enable = true;
-            #     enableNushellIntegration = true;
-            # };
+            jujutsu = {
+              enable = true;
+              settings = {
+                user = {
+                  name = "Tim Chmielecki";
+                  email = "me@timcki.com";
+                };
+                "template-aliases" = {
+                  "format_timestamp(timestamp)" = "timestamp.ago()";
+                };
+                ui = {
+                  default-command = "log";
+                  log-synthetic-elided-nodes = true;
+                  pager = "less -RFX";
+                  merge-editor = ":builtin";
+                };
+                signing = {
+                  behavior = "own";
+                  backend = "ssh";
+                  key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIF3zNKM+CpNS5isd8MkCbPy6qTYbPlbVeyqm3hG5FYww";
+                  "backends.ssh.allowed-signers" = "/Users/tim/.ssh/git_allowed_signers";
+                  "backends.ssh.program" = "/Applications/1Password.app/Contents/MacOS/op-ssh-sign";
+                };
+                aliases = {
+                  "log-recent" = [ "log" "-r" "default() & recent()" ];
+                  tug = [ "bookmark" "move" "--from" "closest_bookmark(@-)" "--to" "@-" ];
+                  c = [ "commit" ];
+                  ci = [ "commit" "--interactive" ];
+                  e = [ "edit" ];
+                  i = [ "git" "init" "--colocate" ];
+                  nb = [ "bookmark" "create" "-r" "@-" ];
+                  pull = [ "git" "fetch" ];
+                  push = [ "git" "push" "--allow-new" ];
+                  r = [ "rebase" ];
+                  s = [ "squash" ];
+                  si = [ "squash" "--interactive" ];
+                };
+                "revset-aliases" = {
+                  "closest_bookmark(to)" = "heads(::to & bookmarks())";
+                  "immutable_heads()" = "builtin_immutable_heads() & remote_bookmarks()";
+                  "recent()" = "committer_date(after:\"3 months ago\")";
+                };
+                git = {
+                  push-new-bookmarks = true;
+                };
+                snapshot = {
+                  max-new-file-size = "10MiB";
+                };
+              };
+            };
 
             starship = {
                 enable = true;
